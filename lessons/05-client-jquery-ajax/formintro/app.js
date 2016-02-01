77dd1f3122850224f7ef36a430a7b429fad75582
joeylmaalouf <joeylmaalouf@gmail.com>
2016-02-01 11:30:00 -0500

    form intro

---
+++ lessons/05-client-jquery-ajax/formintro/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var path = require("path");
+var exphbs  = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+var index = require("./routes/index");
+var getCat = require("./routes/getCat.js");
+
+var app = express();
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/", index.home);
+app.get("/getCat", getCat.getCatGET);
+app.post("/getCat", getCat.getCatPOST);
+
+app.listen(3000);
