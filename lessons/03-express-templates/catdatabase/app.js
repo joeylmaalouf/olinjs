67f31cb7e80e92d6298e131771a2530e35dc1c7d
Radmer van der Heyde <radmer.vanderheyde@students.olin.edu>
2016-02-04 12:36:02 -0500

    Done Grading. Good Workgit add app.js

--- lessons/03-express-templates/catdatabase/app.js
+++ lessons/03-express-templates/catdatabase/app.js
@@ -1,17 +1,19 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-var path = require("path");
-var index = require("./routes/index");
+//Ties in modules 
+var express = require("express"); //Handles requests, responses, and many other things for us
+var exphbs = require("express-handlebars"); //Our templater
+var path = require("path"); //Makes it easier to describe location of things
+//Ties in your modules
+var index = require("./routes/index"); 
 var cats = require("./routes/cats");
 
-var app = express();
+var app = express(); //Creates server
 
-var PORT = process.env.PORT || 3000;
-
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
-app.set("view engine", "handlebars");
-app.use(express.static(path.join(__dirname, "public")));
-
+var PORT = process.env.PORT || 3000; //Use environment variable or 3000
+//Set up middleware for express to use. 
+app.engine("handlebars", exphbs({defaultLayout: "main"})); //allows us to use our handlebars to template
+app.set("view engine", "handlebars"); //render will use handlebars
+app.use(express.static(path.join(__dirname, "public"))); //make public file reachable for requests
+//The routes your server handles
 app.get("/", index.home);
 app.get("/cats", cats.list);
 app.get("/cats/new", cats.new);
