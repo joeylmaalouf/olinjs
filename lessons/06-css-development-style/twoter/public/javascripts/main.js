0995f9038d610d401ded0d9dfd5491341bde971f
joeylmaalouf <joeylmaalouf@gmail.com>
2016-02-13 21:11:10 -0500

    looks much better now

--- lessons/06-css-development-style/twoter/public/javascripts/main.js
+++ lessons/06-css-development-style/twoter/public/javascripts/main.js
@@ -17,22 +17,26 @@ var logOut = function () {
 };
 
 var makeTwote = function () {
+  var username = $("div#logOut span#username").html();
+  var twote = $("div#makeTwote textarea#twote").val();
+  console.log(username);
+  console.log(twote);
   $.post("/makeTwote", {
-    "author": $("div#logOut span#username").html(),
-    "text": $("div#makeTwote textarea#twote").val(),
+    "author": username,
+    "text": twote,
     "date": new Date()
   }).done(function (data, status) {
     $("table#twotes tbody").prepend(
-      "      <tr id=\"" + data._id + "\">\n" +
+      "      <tr id=\"" + data._id + "\" class=\"content\">\n" +
       "        <td>\n" +
-      "          <span id=\"author\" onclick=\"filterTwotes('" + data.author + "');\">" + data.author + "</span>, <span id=\"date\">" + formatDate(data.date) + "</span>:\n" +
-      "          <input type=\"button\" value=\"X\" onclick=\"deleteTwote('" + data._id + "');\"><br>\n" +
+      "          <span id=\"author\" class=\"clickable\" onclick=\"filterTwotes('" + data.author + "');\">" + data.author + "</span>, <span id=\"date\">" + formatDate(data.date) + "</span>:\n" +
+      "          <input type=\"button\" value=\"X\" class=\"right\" onclick=\"deleteTwote('" + data._id + "');\"><br>\n" +
       "          <span id=\"text\">" + data.text + "</span><br>\n" +
       "        </td>\n" +
       "      </tr>"
     );
     $("ul#users li#" + data.author).remove();
-    $("ul#users").prepend("<li id=\"" + data.author + "\">" + data.author + "</li>");
+    $("ul#users").prepend("<li id=\"" + data.author + "\" class=\"clickable\"onclick=\"filterTwotes('" + data.author + "', false);\">" + data.author + "</li>");
   }).error(function (data, status) {
     console.log("Error: " + data);
   });
@@ -54,11 +58,11 @@ var deleteTwote = function (id) {
 var filterTwotes = function (author, reset) {
   $("table#twotes tbody tr").each(function (index, element) {
     element = $(element);
-    if (reset || element.find("td span#author").html() === author) {
-      element.show();
+    if (reset || element.find("td span#author").html() !== author) {
+      element.removeClass("highlighted").addClass("content");
     }
     else {
-      element.hide();
+      element.removeClass("content").addClass("highlighted");
     }
   });
 };
