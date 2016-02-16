0f538044375cb971ce23265a67330f1a97feaf12
joeylmaalouf <joeylmaalouf@gmail.com>
2016-02-16 10:18:48 -0500

    started updating twoter with auth

---
+++ lessons/07-apis-debugging/twoter/app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var path = require("path");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+var index = require("./routes/index");
+
+var PORT = process.env.PORT || 3000;
+var MONGOURI = process.env.MONGOURI || "mongodb://localhost/test";
+
+var app = express();
+
+mongoose.connect(MONGOURI);
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", index.home);
+
+app.post("/makeTwote", index.makeTwote);
+app.post("/deleteTwote", index.deleteTwote);
+
+app.listen(PORT, function() {
+  console.log("Application running on port: " + PORT);
+});
