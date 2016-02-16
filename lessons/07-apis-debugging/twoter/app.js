dea55f34b2af5d9c9ceb4b7dbb8d1056d3ef6bda
joeylmaalouf <joeylmaalouf@gmail.com>
2016-02-16 16:26:45 -0500

    configuring app.js

--- lessons/07-apis-debugging/twoter/app.js
+++ lessons/07-apis-debugging/twoter/app.js
@@ -3,13 +3,34 @@ var exphbs = require("express-handlebars");
 var path = require("path");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
-
+var passport = require("passport");
+var GithubStrategy = require("passport-github").Strategy;
+var config = require("./oauth.js");
 var index = require("./routes/index");
 
 var PORT = process.env.PORT || 3000;
 var MONGOURI = process.env.MONGOURI || "mongodb://localhost/test";
 
 var app = express();
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GithubStrategy({
+  clientID: config.github.clientID,
+  clientSecret: config.github.clientSecret,
+  callbackURL: config.github.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
 
 mongoose.connect(MONGOURI);
 
@@ -18,13 +39,33 @@ app.set("view engine", "handlebars");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", index.home);
 
-app.post("/logIn", index.logIn);
-app.post("/logOut", index.logOut);
 app.post("/makeTwote", index.makeTwote);
 app.post("/deleteTwote", index.deleteTwote);
+
+app.get(
+  "/auth/github",
+  passport.authenticate("github"),
+  function (req, res) {}
+);
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/" }),
+  function(req, res) {
+    res.redirect("/");
+  }
+);
+app.get(
+  "/logout",
+  function(req, res) {
+    req.logout();
+    res.redirect("/");
+  }
+);
 
 app.listen(PORT, function() {
   console.log("Application running on port: " + PORT);
