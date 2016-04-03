1d0382a69135ba05c9caedc20fbca589d4992088
Sarah Walters <swalters4925@gmail.com>
2016-04-02 21:40:59 -0400

    Homework 8 feedback

--- lessons/08-tests-tasks/orderburgers/tests/server/appSpec.js
+++ lessons/08-tests-tasks/orderburgers/tests/server/appSpec.js
@@ -23,8 +23,16 @@ describe("The server", function () {
   it("should respond with the correct HTML on GET /ingredients", function (done) {
     request(app)
       .get("/ingredients")
-      .expect("Content-Type", "text/html; charset=utf-8")
-      .expect("Content-Length", "5402", done);
+      .expect("Content-Type", "text/html; charset=utf-8", done);
+      /* We pointed you guys towards checking content length in the cat app example,
+         and we were kind of wrong to do so. Fine to check content length on pages with
+         fixed contents, but most pages have variable contents -- e.g. the cats page
+         varies in length depending on how many cats you have, their names, their ages, etc;
+         same for this ingredients page. I'm guessing 5402 was the correct content length
+         for the page as it looked when you wrote the test -- but my database state is different
+         from yours, so it fails for me. Best to take out content length altogether here -- apologies
+         for the misleading example!
+      .expect("Content-Length", "5402", done);*/
   });
 
   it("should return 200 OK on GET /order", function (done) {
@@ -35,8 +43,8 @@ describe("The server", function () {
   it("should respond with the correct HTML on GET /order", function (done) {
     request(app)
       .get("/order")
-      .expect("Content-Type", "text/html; charset=utf-8")
-      .expect("Content-Length", "2574", done);
+      .expect("Content-Type", "text/html; charset=utf-8", done); // same here
+      // .expect("Content-Length", "2574", done);
   });
 
   it("should return 200 OK on GET /kitchen", function (done) {
@@ -48,8 +56,10 @@ describe("The server", function () {
     request(app)
       .get("/kitchen")
       .expect("Content-Type", "text/html; charset=utf-8")
-      .expect("Content-Length", "481", done);
+      .expect("Content-Length", "481", done); // probably same here? kitchen page is variable
   });
+
+  // How could you have tested the POST routes?
 
   it("should return 404 on GET /notaroute", function (done) {
     request(app)
